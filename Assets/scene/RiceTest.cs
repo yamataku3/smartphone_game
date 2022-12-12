d8c559bca292450f2edac57dbadc964ddaf80bf1
yamataku3 <imuka06x17@keio.jp>
2022-12-12 17:00:06 +0900

    Animationを作成

--- Assets/scene/RiceTest.cs
+++ Assets/scene/RiceTest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class RiceCreate : MonoBehaviour
+public class RiceTest : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] GameObject rice;
@@ -13,19 +13,20 @@ public class RiceCreate : MonoBehaviour
         // Cube�v���n�u��GameObject�^�Ŏ擾
         //GameObject obj = (GameObject)Resources.Load("rice");
         // Cube�v���n�u�����ɁA�C���X�^���X�𐶐��A
-        for(int count = 0; count < 100;)
+        for(int count=0; count<100;)
         {
             float rand_x = Random.Range(-0.8f, 0.8f);
             float rand_y = Random.Range(-0.8f, 0.8f);
-            if(rand_x*rand_x + rand_y*rand_y < 0.6)
+            if(rand_x*rand_x + rand_y*rand_y < 0.64)
             {
-                Instantiate(rice, new Vector3(rand_x, rand_y, 0.5f), Quaternion.identity);
+                Instantiate(rice, new Vector3(rand_x, rand_y, 0.0f), Quaternion.identity);
                 count++;
             }
             
         }
         
     }
+    
     // Update is called once per frame
     void Update()
     {
