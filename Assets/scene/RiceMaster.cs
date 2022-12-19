f84130396848b4cd56140a9983e26c0caf9ba3a3
yamataku3 <imuka06x17@keio.jp>
2022-12-19 17:50:28 +0900

    リファクタリング完了

--- Assets/scene/RiceMaster.cs
+++ Assets/scene/RiceMaster.cs
@@ -7,27 +7,16 @@ using TMPro;
 
 public class RiceMaster : MonoBehaviour
 {
-    /*
-    start: 米生成
-    update: 
-    米を動かすべきかどうか
-
-    */
+    
     // Start is called before the first frame update
     [SerializeField] GameObject rice;
-    [SerializeField] TextMeshProUGUI chahan_text;
     int riceN = 1000;
     GameObject[] rices;
-    getSensor sensorscript;
     public Material color1;
     public Material color2;
-    float delay_time = 0.0f;
-    bool move_rice_flag = true;
-    float option = 0.0f;
-    int fly_level = 0;
-
     void Start()
     {
+        // generate ri
         rices = new GameObject[riceN];
         for (int count = 0; count < riceN;)
         {
@@ -40,120 +29,45 @@ public class RiceMaster : MonoBehaviour
                 count++;
             }
         }
-        GameObject fryingpan = GameObject.Find("fryingpan");
-        sensorscript = fryingpan.GetComponent<getSensor>();
     }
+
+
     // Update is called once per frame
     void Update(){
-        if (move_rice_flag){          
-            if (sensorscript.move_option == 3){
-                Debug.Log("large" + fly_level);
-                option = 2.0f;
-                move_rice_flag = false;
-                delay_time = 1.0f;
-                Invoke(nameof(forceToRiceStart), delay_time);
-                Invoke(nameof(forceToRice), delay_time + 0.2f);
-                Invoke(nameof(forceToRiceEnd), delay_time + 0.4f);
-                sensorscript.move_option = 0;
-                chahan_text.text = "Too strong ... \nTry again!";
-                Invoke(nameof(changeFlag), 2.5f);
-            }
-            else if (sensorscript.move_option == 2){   
-                Debug.Log("middle" + fly_level);
-                option = 1.5f;
-                move_rice_flag = false;
-                delay_time = 0.8f;
-                Invoke(nameof(forceToRiceStart), delay_time);
-                Invoke(nameof(forceToRice), delay_time + 0.2f);
-                Invoke(nameof(forceToRiceEnd), delay_time + 0.4f);
-                sensorscript.move_option = 0;
-                chahan_text.text = "Good!";
-                Invoke(nameof(changeFlag), 2.5f);
-                if (fly_level == 0)
-                {
-                    Invoke(nameof(fryRice1), 2.5f);
-                    fly_level++;
-                }else if (fly_level == 1)
-                {
-                    Invoke(nameof(fryRice2), 2.5f);
-                    fly_level++;
-                }else if (fly_level == 2)
-                {
-                    Debug.Log("finish!!!");
-                    Invoke(nameof(changeScene), 2.5f);
-                }
-                
-            }
-            else if (sensorscript.move_option == 1){
-                move_rice_flag = false;
-                option = 1.0f;
-                Debug.Log("small" + fly_level);
-                delay_time = 0.6f;
-                Invoke(nameof(forceToRiceStart), delay_time);
-                Invoke(nameof(forceToRice), delay_time + 0.2f);
-                Invoke(nameof(forceToRiceEnd), delay_time + 0.4f);
-                sensorscript.move_option = 0;
-                Invoke(nameof(changeFlag), 2.5f);
-                chahan_text.text = "Too weak ... \nTry again!";
-            }
-            else{
-                option = 0.0f;
-            }
-            
-        }
     }
     
-    void changeScene()
+    
+    public IEnumerator riceColorChange(float delay, int state)
     {
-        SceneManager.LoadScene("endScene");
-    }
-    void fryRice1()
-    {
+        Debug.Log("test");
+        //delay秒待つ
+        Material rice_color;
+        yield return new WaitForSeconds(delay);
+        if(state == 0){
+            rice_color = color1;
+        }else{
+            rice_color = color2;  
+        }
         for (int i = 0; i < riceN; i++)
         {
-            rices[i].GetComponent<Renderer>().material.color = color1.color;
+            rices[i].GetComponent<Renderer>().material.color = rice_color.color;
         }
     }
 
-    void fryRice2()
-    {
+    public IEnumerator forceToRice(float delay, bool random_flag, float option, float x, float y, float z){
+        Vector3 force;
+        yield return new WaitForSeconds(delay);
         for (int i = 0; i < riceN; i++)
         {
-            rices[i].GetComponent<Renderer>().material.color = color2.color;
-        }
-    }
-
-    void forceToRice(){
-        for (int i = 0; i < riceN; i++)
-        {
-            float force_x = Random.Range(-0.3f, 0.3f) * option;
-            float force_y = Random.Range(-0.3f, 0.3f) * option;
-            float force_z = Random.Range(2.0f, 4.0f) * option;
-            Vector3 force = new Vector3(force_x, force_y, force_z);  // 力を設定
+            if(random_flag){
+                float force_x = Random.Range(-0.3f, 0.3f);
+                float force_y = Random.Range(-0.3f, 0.3f);
+                float force_z = Random.Range(2.0f, 4.0f);
+                force = new Vector3(force_x * option , force_y * option , force_z * option);  // 力を設定
+            }else{
+                force = new Vector3(x, y, z);  // 力を設定
+            }
             rices[i].GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
         }
-    }
-    void forceToRiceStart()
-    {
-        Vector3 force_start = new Vector3(-1.8f, 0, 0);
-        for (int i = 0; i < riceN; i++)
-        {
-            rices[i].GetComponent<Rigidbody>().AddForce(force_start, ForceMode.Impulse);
-        }
-    }
-    void forceToRiceEnd()
-    {
-        Vector3 force_end = new Vector3(0.5f, 0, 0);
-        
-        for (int i = 0; i < riceN; i++)
-        {
-            rices[i].GetComponent<Rigidbody>().AddForce(force_end, ForceMode.Impulse);
-        }
-    }
-    public void changeFlag()
-    {
-        move_rice_flag = true;
-        int times = 3 - fly_level;
-        chahan_text.text = "Shake your smartphone\n(" + times + "times)";
     }
 }
