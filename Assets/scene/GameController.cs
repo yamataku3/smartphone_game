31adeb9bc5bb4027ee39ba64bad7368ed26ef114
TakayanagiNaoto <naoto20221106@keio.jp>
2022-12-27 15:32:00 +0900

    食材を追加する（肉とネギ）

--- Assets/scene/GameController.cs
+++ Assets/scene/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using TMPro;
 
 // get sensor data
@@ -19,12 +20,14 @@ public class GameController: MonoBehaviour
     public int move_option = 0; // 動かし方を管理する変数
     public int game_phase_management = 0; //ゲームのフェーズ管理
     [SerializeField] TextMeshProUGUI chahan_text;
+    [SerializeField] Button put_onion_button;
+    [SerializeField] Button put_meat_button;
     public float force_ratio;
     float delay_time = 0.0f;
     // Animator
     private Animator anim;
-    // RiceMaster
-    private RiceMaster riceMasterScript;
+    // FoodMaster
+    private FoodMaster foodMasterScript;
     public int count_lower = 0;//焦げた回数をカウント
     // Start is called before the first frame update
 
@@ -37,9 +40,11 @@ public class GameController: MonoBehaviour
         Input.compass.enabled = true; 
         // get the animator
         anim = gameObject.GetComponent<Animator>();
-        // get the RiceMaster
-        riceMasterScript= GameObject.Find("GameObject").GetComponent<RiceMaster>();
-
+        // get the FoodMaster
+        foodMasterScript= GameObject.Find("GameObject").GetComponent<FoodMaster>();
+        //食材を追加するためのボタン
+        put_onion_button.onClick.AddListener(GreenOnionButtonOnClick);
+        put_meat_button.onClick.AddListener(MeatButtonOnClick);
     }
     // Update is called once per frame
     void FixedUpdate()
@@ -56,9 +61,9 @@ public class GameController: MonoBehaviour
                 move_option = 3;
                 delay_time = 1.0f;
                 force_ratio = 2.0f;
-                StartCoroutine(riceMasterScript.forceToRice(delay_time, false, force_ratio, -1.8f, 0, 0));
-                StartCoroutine(riceMasterScript.forceToRice(delay_time + 0.2f, true, force_ratio, 0, 0, 0));
-                StartCoroutine(riceMasterScript.forceToRice(delay_time + 0.4f, false, force_ratio, 0.5f, 0, 0));
+                StartCoroutine(foodMasterScript.forceToRice(delay_time, false, force_ratio, -1.8f, 0, 0));
+                StartCoroutine(foodMasterScript.forceToRice(delay_time + 0.2f, true, force_ratio, 0, 0, 0));
+                StartCoroutine(foodMasterScript.forceToRice(delay_time + 0.4f, false, force_ratio, 0.5f, 0, 0));
                 
                 anim.SetBool("AnimationFlag_large", true);
                 StartCoroutine(changeAnimatorState(1.5f, "AnimationFlag_large", false));
@@ -68,9 +73,9 @@ public class GameController: MonoBehaviour
                 move_option = 2;
                 delay_time = 0.8f;
                 force_ratio = 1.3f;
-                StartCoroutine(riceMasterScript.forceToRice(delay_time, false, force_ratio, -1.8f, 0, 0));
-                StartCoroutine(riceMasterScript.forceToRice(delay_time + 0.2f, true, force_ratio, 0, 0, 0));
-                StartCoroutine(riceMasterScript.forceToRice(delay_time + 0.4f, false, force_ratio, 0.5f, 0, 0));
+                StartCoroutine(foodMasterScript.forceToRice(delay_time, false, force_ratio, -1.8f, 0, 0));
+                StartCoroutine(foodMasterScript.forceToRice(delay_time + 0.2f, true, force_ratio, 0, 0, 0));
+                StartCoroutine(foodMasterScript.forceToRice(delay_time + 0.4f, false, force_ratio, 0.5f, 0, 0));
                 
                 anim.SetBool("AnimationFlag_middle", true);
                 StartCoroutine(changeAnimatorState(1.5f, "AnimationFlag_middle", false));
@@ -78,11 +83,11 @@ public class GameController: MonoBehaviour
                 chahan_text.text = "Good!";
                 if(game_phase_management == 2){                   
                     //シーン遷移
-                    score = riceMasterScript.calculatingScore(count_lower);
+                    score = foodMasterScript.calculatingScore(count_lower);
                     Debug.Log(score);
                     StartCoroutine(changeScene(2.5f));
                 }else{
-                    StartCoroutine(riceMasterScript.riceColorChange(2.5f, game_phase_management));
+                    StartCoroutine(foodMasterScript.riceColorChange(2.5f, game_phase_management));
                 }
                 game_phase_management++;
                 
@@ -95,9 +100,9 @@ public class GameController: MonoBehaviour
                 move_option = 1;
                 delay_time = 0.6f;
                 force_ratio = 1.0f;
-                StartCoroutine(riceMasterScript.forceToRice(delay_time, false, force_ratio, -1.8f, 0, 0));
-                StartCoroutine(riceMasterScript.forceToRice(delay_time + 0.2f, true, force_ratio, 0, 0, 0));
-                StartCoroutine(riceMasterScript.forceToRice(delay_time + 0.4f, false, force_ratio, 0.5f, 0, 0));
+                StartCoroutine(foodMasterScript.forceToRice(delay_time, false, force_ratio, -1.8f, 0, 0));
+                StartCoroutine(foodMasterScript.forceToRice(delay_time + 0.2f, true, force_ratio, 0, 0, 0));
+                StartCoroutine(foodMasterScript.forceToRice(delay_time + 0.4f, false, force_ratio, 0.5f, 0, 0));
                 anim.SetBool("AnimationFlag_small", true);
                 StartCoroutine(changeAnimatorState(1.5f, "AnimationFlag_small", false));
                 chahan_text.text = "Too weak ... \nTry again!";
@@ -121,6 +126,18 @@ public class GameController: MonoBehaviour
         yield return new WaitForSeconds(delay);
         SceneManager.LoadScene("endScene");
     }
+
+    public void GreenOnionButtonOnClick()
+    {
+        Debug.Log("onion");
+        foodMasterScript.put_greenonion();
+    }
+    public void MeatButtonOnClick()
+    {
+        Debug.Log("meat");
+        foodMasterScript.put_meat();
+    }
+
 }
 
 
