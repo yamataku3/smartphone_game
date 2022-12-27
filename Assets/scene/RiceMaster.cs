358cb66082d7ee0847f4362563a02fb1ec870e69
TakayanagiNaoto <naoto20221106@keio.jp>
2022-12-27 13:00:29 +0900

    スコア計算の自走とそれに伴うBaseの当たり判定の変更

--- Assets/scene/RiceMaster.cs
+++ Assets/scene/RiceMaster.cs
@@ -11,21 +11,28 @@ public class RiceMaster : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] GameObject rice;
     int riceN = 1000;
+    public List<GameObject> ingredient_list = new List<GameObject>();
+
     GameObject[] rices;
     public Material color1;
     public Material color2;
+    public double score;
+    public DetectionFallenObject detectionFallenObjectScript;
+
     void Start()
     {
-        // generate ri
-        rices = new GameObject[riceN];
+        // generate rice
+        //rices = new GameObject[riceN];
         for (int count = 0; count < riceN;)
         {
             float rand_x = Random.Range(-0.8f, 0.8f);
             float rand_y = Random.Range(-0.8f, 0.8f);
             if(rand_x*rand_x + rand_y*rand_y < 0.6)
             {
-                rices[count] = Instantiate(rice, new Vector3(rand_x, rand_y, 0.7f), Quaternion.identity);
-                rices[count].name = "Rice" + count.ToString();
+                ingredient_list.Add(Instantiate(rice, new Vector3(rand_x, rand_y, 0.7f), Quaternion.identity));
+                ingredient_list[count].name = "Rice" + count.ToString();
+                //rices[count] = Instantiate(rice, new Vector3(rand_x, rand_y, 0.7f), Quaternion.identity);
+                //rices[count].name = "Rice" + count.ToString();
                 count++;
             }
         }
@@ -50,7 +57,8 @@ public class RiceMaster : MonoBehaviour
         }
         for (int i = 0; i < riceN; i++)
         {
-            rices[i].GetComponent<Renderer>().material.color = rice_color.color;
+            ingredient_list[i].GetComponent<Renderer>().material.color = rice_color.color;
+            //rices[i].GetComponent<Renderer>().material.color = rice_color.color;
         }
     }
 
@@ -67,7 +75,15 @@ public class RiceMaster : MonoBehaviour
             }else{
                 force = new Vector3(x, y, z);  // 力を設定
             }
-            rices[i].GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+            ingredient_list[i].GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+            //rices[i].GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
         }
+    }
+
+    //得点を計算
+    public double calculatingScore(int i)
+    {
+        score = 100 - (double)detectionFallenObjectScript.count_fallen_object / (double)ingredient_list.Count * 100 - 5 * i;
+        return score;
     }
 }
