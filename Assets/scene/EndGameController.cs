d6100609ed47aae398ef74c394d81c79f940bc89
TakayanagiNaoto <naoto20221106@keio.jp>
2023-01-17 16:44:20 +0900

    効果音を複数に対応

--- Assets/scene/EndGameController.cs
+++ Assets/scene/EndGameController.cs
@@ -11,14 +11,13 @@ public class EndGameController : MonoBehaviour
     int resultScore, resultLower, resultUpper, resultMiddle, ingredientCount, detectionCount, Minutes, Seconds;
     [SerializeField] Text timeText, chahanNameText, strongText, weakText, percentText, scoreText;
     [SerializeField] Button RetryButton, TitleButton;
-  
-    // Audio clip
-    AudioSource audio_source_se;
-    public AudioClip audio_clip_se;
+
+    [SerializeField] private AudioSource audioSource_don, audioSource_dora;
+    [SerializeField] private AudioClip audioClip_don, audioClip_dora;
+
     // Start is called before the first frame update
     void Start()
     {
-        audio_source_se = GetComponent<AudioSource>();
         resultScore = PlayerPrefs.GetInt("score");
         resultUpper = PlayerPrefs.GetInt("count_upper");
         resultLower = PlayerPrefs.GetInt("count_lower");
@@ -45,6 +44,8 @@ public class EndGameController : MonoBehaviour
         Debug.Log("DetectionCount:" + detectionCount);
         Debug.Log("Minutes:" + Minutes);
         Debug.Log("Seconds:" + Seconds);
+
+        StartCoroutine(display_fried_rice_name(3.0f, "finish_chahan", true));
     }
 
     // Update is called once per frame
@@ -63,14 +64,13 @@ public class EndGameController : MonoBehaviour
     {
         //delay秒待つ
         yield return new WaitForSeconds(delay);
-        GameObject.Find(name).SetActive(true);
-        if (is_score){
-            audio_source_se.PlayOneShot(audio_clip_se);
-        }else{
-            audio_source_se.PlayOneShot(audio_clip_se);
-        }
         
-
+        if (is_score){
+            audioSource_dora.PlayOneShot(audioClip_dora);
+        }else{
+            audioSource_don.PlayOneShot(audioClip_don);
+        }
+        GameObject.Find(name).SetActive(true);
     }
 
 
